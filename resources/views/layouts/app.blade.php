af8c3019af523fd4fef0f92e6670d2bd7d402d3d
unknown <zulfanlutfiansyah212@gmail.com>
2026-01-28 11:07:37 +0700

    Fix: ensure vendor folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/font-awesome.min.css" rel='stylesheet' type='text/css'> 
+    <link href="/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/jquery.dataTables.css" rel="stylesheet">
-    <link href="/css/dataTables.bootstrap.css" rel="stylesheet">
-    <link href="/css/selectize.css" rel="stylesheet">
+    <link href="/css/dataTables.bootstrap.min.css" rel="stylesheet">
     <link href="/css/selectize.bootstrap3.css" rel="stylesheet">
+    <link href="/css/selectize.css" rel="stylesheet">
+
 
     <!-- Scripts -->
     <script>
@@ -27,78 +28,78 @@
     </script>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        @if (Auth::check())
+                            {!! Html::smartNav(url('/home'), 'Dashboard') !!}
+                            
+                        @endif
+                        @role('admin')
+                            {!! Html::smartNav(route('authors.index'), 'Penulis') !!}
+                            {!! Html::smartNav(route('books.index'), 'Buku') !!}
+                            {!! Html::smartNav(route('members.index'), 'Member') !!}
+                            {!! Html::smartNav(route('statistics.index'), 'Peminjaman') !!}
+                        @endrole
+                        @if (auth()->check())
+                            {!! Html::smartNav(url('/settings/profile'), 'Profil') !!}
+                        @endif
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Daftar</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ url('/settings/password') }}"><i class="fa fa-btn fa-lock"></i> Ubah Password</a></li>
+                                    <li>
+                                        <a href="{{ url('/logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
             </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!--Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    @if (Auth::check())
-                        {!! Html::smartNav(url('/home'), 'Dashboard') !!}
-                    @endif
-                    @role('admin')
-                        {!! Html::smartNav(route('authors.index'), 'Penulis') !!}
-                        {!! Html::smartNav(route('books.index'), 'Buku') !!}
-                        {!! Html::smartNav(route('members.index'), 'Member') !!}
-                        {!! Html::smartNav(route('statistics.index'), 'Peminjaman') !!}
-                    @endrole
-                    @if (auth()->check())
-                        {!! Html::smartNav(url('/settings/profile'), 'Profil') !!}
-                    @endif
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Daftar</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-
-                                <li><a href="{{ url('/settings/password') }}"><i class="fa fa-btn fa-lock"></i> Ubah Password</a></li>
-                                <li>
-                                    <a href="{{ url('/logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    @include('layouts._flash')
-
-    @yield('content')
+        </nav>
+        @include('layouts._flash')
+        @yield('content')
+    </div>
 
     <!-- Scripts -->
     <script src="/js/jquery-3.1.0.min.js"></script>
